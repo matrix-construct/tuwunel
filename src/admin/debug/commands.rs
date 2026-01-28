f9805067b52fe731bdbab2d2eb41f8fe9a3f072a
dasha_uwu <dasha@linuxping.win>
2026-01-28 18:58:36 +0500

    Fix formatting output of `admin debug get-retained-pdu` [ci skip]

--- src/admin/debug/commands.rs
+++ src/admin/debug/commands.rs
@@ -1034,7 +1034,7 @@ pub(super) async fn get_retained_pdu(&self, event_id: OwnedEventId) -> Result {
 
 	let text = serde_json::to_string_pretty(&pdu)?;
 
-	self.write_str(&format!("Original PDU:\n```json\n{text}```"))
+	self.write_str(&format!("Original PDU:\n```json\n{text}\n```"))
 		.await?;
 
 	Ok(())
