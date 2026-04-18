645e3750cdfde36a8226cc876b4fcf25c06c0c34
dasha_uwu <dasha@linuxping.win>
2026-04-18 05:34:06 +0500

    Auto download media [ci skip]

--- src/service/rooms/timeline/mod.rs
+++ src/service/rooms/timeline/mod.rs
@@ -24,8 +24,8 @@ use futures::{
 	pin_mut,
 };
 use ruma::{
-	CanonicalJsonObject, EventId, MilliSecondsSinceUnixEpoch, OwnedEventId, OwnedRoomId, RoomId,
-	UserId, api::Direction, events::room::encrypted::Relation,
+	CanonicalJsonObject, EventId, MilliSecondsSinceUnixEpoch, OwnedEventId, OwnedMxcUri,
+	OwnedRoomId, RoomId, UserId, api::Direction, events::room::encrypted::Relation,
 };
 use serde::Deserialize;
 /// Re-exports the typed and raw persistent timeline identifiers.
@@ -99,6 +99,11 @@ struct ExtractRelatesToEventId {
 #[derive(Deserialize)]
 struct ExtractBody {
 	body: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct ExtractUrl {
+	url: Option<OwnedMxcUri>,
 }
 
 type RoomMutexMap = MutexMap<OwnedRoomId, ()>;
